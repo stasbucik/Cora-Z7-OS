d0ef556f19377eed6fbef32cfef5e840193b633c
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-02-05 15:29:01 +0100

    upgrade 24.2: Reset to template
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
+++ project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
@@ -1,5 +1,16 @@
-#include <configs/zynq-common.h>
-#include <configs/platform-auto.h>
-
+#if defined(CONFIG_MICROBLAZE)
+#include <configs/microblaze-generic.h>
 #define CONFIG_SYS_BOOTM_LEN 0xF000000
-#undef CONFIG_SYS_BOOTMAPSZ
+#endif
+#if defined(CONFIG_ARCH_ZYNQ)
+#include <configs/zynq-common.h>
+#endif
+#if defined(CONFIG_ARCH_ZYNQMP)
+#include <configs/xilinx_zynqmp.h>
+#endif
+#if defined(CONFIG_ARCH_VERSAL)
+#include <configs/xilinx_versal.h>
+#endif
+#if defined(CONFIG_ARCH_VERSAL_NET)
+#include <configs/xilinx_versal_net.h>
+#endif
--- project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
+++ project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
@@ -1,4 +1,4 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
-SRC_URI += "file://devtool-fragment.cfg"
-
+SRC_URI:append = " file://bsp.cfg"
+KERNEL_FEATURES:append = " bsp.cfg"
