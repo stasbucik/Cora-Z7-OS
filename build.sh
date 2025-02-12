ad42029f9522e9e11eea413e2de5dce3cb319a42
stasbucik <53035783+stasbucik@users.noreply.github.com>
2025-02-12 10:35:38 +0100

    Add wic file to build process
    Signed-off-by: stasbucik <53035783+stasbucik@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -e
 
 petalinux-build
 petalinux-package --boot --fpga project-spec/hw-description/system_wrapper.bit --fsbl images/linux/zynq_fsbl.elf --u-boot --force
+petalinux-package wic -w project-spec/meta-user/conf/rootfs.wks --wic-extra-args="-c xz" -o .
